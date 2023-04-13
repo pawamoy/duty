11b54dab2f36efbac0650b825ed2e3ce73e9afac
Timothée Mazzucotelli <pawamoy@pm.me>
2023-04-13 16:04:33 +0200

    feat: Add callable for `ssort`

---
+++ src/duty/callables/ssort.py
@@ -0,0 +1,38 @@
+"""Callable for [ssort](https://github.com/bwhmather/ssort)."""
+
+from __future__ import annotations
+
+import sys
+
+from failprint.lazy import lazy
+
+
+@lazy(name="ssort")
+def run(
+    *files: str,
+    diff: bool | None = None,
+    check: bool | None = None,
+) -> int:
+    r"""Run `ssort`.
+
+    Parameters:
+        *files: Files to format.
+        diff: Prints a diff of all changes ssort would make to a file.
+        check: Check the file for unsorted statements. Returns 0 if nothing needs to be changed. Otherwise returns 1.
+    """
+    from ssort._main import main as ssort
+
+    cli_args = list(files)
+
+    if diff:
+        cli_args.append("--diff")
+
+    if check:
+        cli_args.append("--check")
+
+    old_sys_argv = sys.argv
+    sys.argv = ["ssort*", *cli_args]
+    try:
+        return ssort()
+    finally:
+        sys.argv = old_sys_argv
