07a62813753a9c0be7b0a5f6c9b710fba0086a72
Timothée Mazzucotelli <dev@pawamoy.fr>
2025-02-10 14:39:50 +0100

    chore: Add copyright notice to Bash completion script

--- src/duty/completions.bash
+++ src/duty/completions.bash
@@ -1,18 +1,18 @@
-# Invoke tab-completion script to be sourced with Bash shell.
-# Known to work on Bash 3.x, untested on 4.x.
+# Taken and adapted from pyinvoke:
+# Copyright (c) 2020 Jeff Forcier.
+# All rights reserved.
 
 _complete_duty() {
     local candidates
 
-    # COMP_WORDS contains the entire command string up til now (including
-    # program name).
-    # We hand it to Invoke so it can figure out the current context: spit back
-    # core options, task names, the current task's options, or some combo.
+    # COMP_WORDS contains the entire command string up til now (including # program name).
+    # We hand it to Invoke so it can figure out the current context:
+    # spit back core options, task names, the current task's options, or some combo.
     candidates=$(duty --complete -- "${COMP_WORDS[@]}")
 
-    # `compgen -W` takes list of valid options & a partial word & spits back
-    # possible matches. Necessary for any partial word completions (vs
-    # completions performed when no partial words are present).
+    # `compgen -W` takes list of valid options & a partial word & spits back possible matches.
+    # Necessary for any partial word completions
+    # (vs. completions performed when no partial words are present).
     #
     # $2 is the current word or token being tabbed on, either empty string or a
     # partial word, and thus wants to be compgen'd to arrive at some subset of
